9aea7a0bd3bd6edb06f0a6c61353e0f5d5dbbf26
ThomasVDD <Thomasvandendries@gmail.com>
2020-04-01 00:51:03 +0200

    COMMUNICATION REWORK

--- Main_metal_prototype_PC_control/Main/Main.ino
+++ Main_metal_prototype_PC_control/Main/Main.ino
@@ -1,7 +1,7 @@
 #include "TimerThree.h"
 #include "PINOUT.h"
 // for debuggin purposes: allows to turn off features
-#define PYTHON 1
+#define PYTHON 0
 #define HARDWARE 0
  
 //---------------------------------------------------------------
