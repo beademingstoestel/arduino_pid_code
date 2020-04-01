12587e10278a7e6b2fb99419abcd665c773549f3
ThomasVDD <Thomasvandendries@gmail.com>
2020-04-01 21:53:38 +0200

    Reformat

--- Main_metal_prototype_PC_control/Main/PINOUT.h
+++ Main_metal_prototype_PC_control/Main/PINOUT.h
@@ -2,7 +2,7 @@
 //#define metal
 #define protoseven
 
-#define hall_sensor_i2c  // comment to use SPI
+//#define hall_sensor_i2c  // comment to use SPI
 
 #ifdef plexi
   #define ENDSWITCH_FULL_PIN 3 // inhale: lower switch
@@ -62,4 +62,30 @@
   #define BME_SPI_MISO 50
   #define BME_SPI_MOSI 51 
   #define BME_SPI_CS 48
+#endif
+
+#ifdef protoGroteSerie
+  #define ENDSWITCH_FULL_PIN 7 // inhale: lower switch
+  #define ENDSWITCH_PUSH_PIN 6 // exhale: upper switch
+  //Motor driver: VNH3SP30-E
+  #define Motor_IN_A 12
+  #define Motor_EN_A 11
+  #define Motor_EN_B 10
+  #define Motor_PWM 9
+  #define Motor_IN_B 8
+  
+  //SPI -> Fixed pins
+  #define BME_SPI_SCK 52 // clk
+  #define BME_SPI_MISO 50 // DO
+  #define BME_SPI_MOSI 51 // DI
+  #define BME_SPI_CS 19 // CS for external barometer
+  
+  #define Speaker_PWM 5;
+  #define Light_PWM 4;
+  #define Fan_PWM 3
+  #define main_supply_voltage A2 // ~batt voltage if no PSU [ANALOG]
+  #define PSU_supply_voltage A3 // detect if PSU is unplugged [ANALOG OR DIGITAL]
+  #define fan_speed A4
+  // TX3 and RX3 to staggered debug header
+  // (pinout debug header: 5V - TX - RX - GND, Square pad = pin1)
 #endif
--- Main_metal_prototype_PC_control/Main/hall_sensor.ino
+++ Main_metal_prototype_PC_control/Main/hall_sensor.ino
@@ -1,156 +1,153 @@
-
-
-
 #ifdef hall_sensor_i2c
-  #include <AS5601.h>
-  
-  //-----------------------------------------------------------------------------------------------------------
-  volatile AS5601 hall_encoder;     // I2C
-  volatile bool HALL_SENSOR_INITIALIZED = false;
-  int startangle = 0;
-  //-----------------------------------------------------------------------------------------------------------
-  bool HALL_SENSOR_INIT()
+#include <AS5601.h>
+
+//-----------------------------------------------------------------------------------------------------------
+volatile AS5601 hall_encoder;     // I2C
+volatile bool HALL_SENSOR_INITIALIZED = false;
+int startangle = 0;
+//-----------------------------------------------------------------------------------------------------------
+bool HALL_SENSOR_INIT()
+{
+  //Serial.println("Setting up encoder");
+  if (!hall_encoder.magnetDetected())
   {
-    //Serial.println("Setting up encoder");
-    if (!hall_encoder.magnetDetected())
-    {
-      Serial.println("Error setting up AS5601 Hall sensor encoder, check magnet or encoder position.");
-      return false; 
-    }
-    HALL_SENSOR_INITIALIZED=true;
-    return true;
+    Serial.println("Error setting up AS5601 Hall sensor encoder, check magnet or encoder position.");
+    return false;
   }
-  //-----------------------------------------------------------------------------------------------------------
-  unsigned int HALL_SENSOR_read()
+  HALL_SENSOR_INITIALIZED = true;
+  return true;
+}
+//-----------------------------------------------------------------------------------------------------------
+unsigned int HALL_SENSOR_read()
+{
+  if (HALL_SENSOR_INITIALIZED)
   {
-    if (HALL_SENSOR_INITIALIZED)
-    {
-      return hall_encoder.getRawAngle();
-    }
+    return hall_encoder.getRawAngle();
+  }
+  return 0;
+}
+//-----------------------------------------------------------------------------------------------------------
+bool HALL_SENSOR_readHall(unsigned int *value)
+{
+  if (HALL_SENSOR_INITIALIZED) {
+    *value = hall_encoder.getRawAngle();
+    return 1;
+  }
+  else {
     return 0;
   }
-  //-----------------------------------------------------------------------------------------------------------
-  bool HALL_SENSOR_readHall(unsigned int *value) 
-  {
-      if (HALL_SENSOR_INITIALIZED){
-        *value = hall_encoder.getRawAngle();
-        return 1;
-      }
-      else{
-        return 0;
-      }
-  }
-  
-  bool HALL_SENSOR_getVolume(float *value){
-    if (HALL_SENSOR_INITIALIZED){
-      int angle = hall_encoder.getRawAngle();
-      if(angle - startangle > 500){
-        angle = (angle - startangle) - 1023;
-        
-      }
-      else{
-        angle = angle - startangle;
-        //Serial.println(angle);
-      }
-  
-      // Convert angle to value with calibration from excel file
-      *value = (float)angle * (float)angle * 0.0757 + (float)angle * 2.2519 + 0.0023;
-      if(*value < 0){
-        *value = 0;
-      }
-      return 1;
+}
+
+bool HALL_SENSOR_getVolume(float *value) {
+  if (HALL_SENSOR_INITIALIZED) {
+    int angle = hall_encoder.getRawAngle();
+    if (angle - startangle > 500) {
+      angle = (angle - startangle) - 1023;
+
     }
-    else{
-      return 0;
+    else {
+      angle = angle - startangle;
+      //Serial.println(angle);
     }
+
+    // Convert angle to value with calibration from excel file
+    *value = (float)angle * (float)angle * 0.0757 + (float)angle * 2.2519 + 0.0023;
+    if (*value < 0) {
+      *value = 0;
+    }
+    return 1;
   }
-  
-  bool HALL_SENSOR_calibrateHall() 
-  {
-      if (HALL_SENSOR_INITIALIZED){
-        startangle = hall_encoder.getRawAngle();
-        startangle = startangle;
-        return 1;
-      }
-      else{
-        return 0;
-      }
+  else {
+    return 0;
   }
+}
+
+bool HALL_SENSOR_calibrateHall()
+{
+  if (HALL_SENSOR_INITIALIZED) {
+    startangle = hall_encoder.getRawAngle();
+    startangle = startangle;
+    return 1;
+  }
+  else {
+    return 0;
+  }
+}
 
 #else
-  #include <AS5040.h>
-  
-  //-----------------------------------------------------------------------------------------------------------
-  volatile AS5040 hall_encoder(AS_SPI_SCK, AS_SPI_CS, AS_SPI_MISO);
-  volatile bool HALL_SENSOR_INITIALIZED = false;
-  int startangle = 0;
-  //-----------------------------------------------------------------------------------------------------------
-  bool HALL_SENSOR_INIT()
+#include <AS5040.h>
+
+//-----------------------------------------------------------------------------------------------------------
+volatile AS5040 hall_encoder(AS_SPI_SCK, AS_SPI_CS, AS_SPI_MISO);
+volatile bool HALL_SENSOR_INITIALIZED = false;
+int startangle = 0;
+//-----------------------------------------------------------------------------------------------------------
+bool HALL_SENSOR_INIT()
+{
+  //Serial.println("Setting up encoder");
+  if (!hall_encoder.begin())
   {
-    //Serial.println("Setting up encoder");
-    if (!hall_encoder.begin())
-    {
-      Serial.println("Error setting up AS5040 Hall sensor encoder");
-      return false; 
-    }
-    HALL_SENSOR_INITIALIZED=true;
-    return true;
+    Serial.println("Error setting up AS5040 Hall sensor encoder");
+    return false;
   }
-  //-----------------------------------------------------------------------------------------------------------
-  unsigned int HALL_SENSOR_read()
+  HALL_SENSOR_INITIALIZED = true;
+  return true;
+}
+//-----------------------------------------------------------------------------------------------------------
+unsigned int HALL_SENSOR_read()
+{
+  if (HALL_SENSOR_INITIALIZED)
   {
-    if (HALL_SENSOR_INITIALIZED)
-    {
-      return hall_encoder.read();
-    }
+    return hall_encoder.read();
+  }
+  return 0;
+}
+//-----------------------------------------------------------------------------------------------------------
+bool HALL_SENSOR_readHall(unsigned int *value)
+{
+  if (HALL_SENSOR_INITIALIZED) {
+    *value = hall_encoder.read();
+    return 1;
+  }
+  else {
     return 0;
   }
-  //-----------------------------------------------------------------------------------------------------------
-  bool HALL_SENSOR_readHall(unsigned int *value) 
-  {
-      if (HALL_SENSOR_INITIALIZED){
-        *value = hall_encoder.read();
-        return 1;
-      }
-      else{
-        return 0;
-      }
-  }
-  
-  bool HALL_SENSOR_getVolume(float *value){
-    if (HALL_SENSOR_INITIALIZED){
-      int angle = hall_encoder.read();
-      if(angle - startangle > 500){
-        angle = (angle - startangle) - 1023;
-        
-      }
-      else{
-        angle = angle - startangle;
-        //Serial.println(angle);
-      }
-  
-      // Convert angle to value with calibration from excel file
-      *value = (float)(angle + 276) * 12.857 - 3979;
-      if(*value < 0){
-        *value = 0;
-      }
-      return 1;
+}
+
+bool HALL_SENSOR_getVolume(float *value) {
+  if (HALL_SENSOR_INITIALIZED) {
+    int angle = hall_encoder.read();
+    if (angle - startangle > 500) {
+      angle = (angle - startangle) - 1023;
+
     }
-    else{
-      return 0;
+    else {
+      angle = angle - startangle;
+      //Serial.println(angle);
     }
+
+    // Convert angle to value with calibration from excel file
+    *value = (float)(angle + 276) * 12.857 - 3979;
+    if (*value < 0) {
+      *value = 0;
+    }
+    return 1;
   }
-  
-  bool HALL_SENSOR_calibrateHall() 
-  {
-      if (HALL_SENSOR_INITIALIZED){
-        startangle = hall_encoder.read();
-        startangle = startangle;
-        return 1;
-      }
-      else{
-        return 0;
-      }
+  else {
+    return 0;
   }
+}
+
+bool HALL_SENSOR_calibrateHall()
+{
+  if (HALL_SENSOR_INITIALIZED) {
+    startangle = hall_encoder.read();
+    startangle = startangle;
+    return 1;
+  }
+  else {
+    return 0;
+  }
+}
 
 #endif
